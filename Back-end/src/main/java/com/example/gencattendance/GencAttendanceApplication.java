8bf9b8bc9e36ef637a9819d5af5b8d7a5fa1bc08
GiridharHUB <scintillantmail@gmail.com>
2023-05-18 17:40:00 +0530

    1st commit

---
+++ Back-end/src/main/java/com/example/gencattendance/GencAttendanceApplication.java
@@ -0,0 +1,13 @@
+package com.example.gencattendance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GencAttendanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GencAttendanceApplication.class, args);
+	}
+
+}
