1792cee82f09feaad257d99b8c403b5fbb32df0e
GiridharHUB <scintillantmail@gmail.com>
2023-05-21 23:02:43 +0530

    dailyAttendance

---
+++ src/Components/DailyAttendance/DailyAttendance.js
@@ -0,0 +1,71 @@
+import React from 'react'
+import {
+    Button,
+    DatePicker,
+    Form,
+    Input,
+    Radio,
+} from 'antd';
+import { useState } from 'react';
+import "../DailyAttendance/DailyAttendance.css"
+
+function DailyAttendance() {
+    const [value, setValue] = useState(1);
+    const onChange = (e) => {
+      console.log('radio checked', e.target.value);
+      setValue(e.target.value);
+    };
+    return (
+        <Form
+            labelCol={{
+                span: 4,
+            }}
+            wrapperCol={{
+                span: 14,
+            }}
+            layout="horizontal"
+            initialValues={{
+                size: "default",
+            }}
+            //onValuesChange={onFormLayoutChange}
+            size={"default"}
+            style={{
+                width: "100%",
+            }}
+        >
+            <div className='inputs'>
+            <Form.Item label="Associate Id">
+                <Input />
+            </Form.Item>
+
+            <Form.Item label="Associate Name">
+                <Input />
+            </Form.Item>
+            <Form.Item label="Project Id">
+                <Input />
+            </Form.Item>
+            <Form.Item label="Project Name">
+                <Input />
+            </Form.Item>
+            <Form.Item label="Project Location">
+                <Input />
+            </Form.Item>
+            <div className='attendance'>
+            <Radio.Group onChange={onChange} value={value}>
+                <Radio value={"Working From Office"}>Office</Radio>
+                <Radio value={"Working From Home"}>WFH</Radio>
+            </Radio.Group>
+            <Form.Item label="Date" >
+                <DatePicker />
+            </Form.Item>
+            </div>
+            </div>
+
+            <Form.Item>
+                <Button>Button</Button>
+            </Form.Item>
+        </Form>
+    )
+}
+
+export default DailyAttendance
