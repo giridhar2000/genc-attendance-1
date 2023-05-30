254eab74280251952302226172cd9a20652a6342
giridhar <giribala2001@gmail.com>
2023-05-30 09:35:53 +0530

    backend

---
+++ Back-end/src/main/java/com/attendanceTracking/controller/AttendanceController.java
@@ -0,0 +1,43 @@
+package com.attendanceTracking.controller;
+
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import com.attendanceTracking.model.AttendanceModel;
+
+import com.attendanceTracking.service.AttendanceService;
+
+@CrossOrigin(origins = "http://localhost:3000/")
+@RestController
+@RequestMapping("/attendance")
+public class AttendanceController {
+	
+	@Autowired
+	private AttendanceService service;
+	
+	/*
+	 * @GetMapping("/show") public List<AttendanceModel> getAll(){ return
+	 * service.showAll(); }
+	 */
+	
+	
+	@GetMapping("/{id}")
+	public Optional<AttendanceModel> getById(@PathVariable("id") Integer id){
+		return service.showAll(id);
+	}
+	
+	@PostMapping("/CREATE")
+	public void addAttendance (@RequestBody AttendanceModel AttendanceModel ) {
+		service.addAttendance(AttendanceModel);
+	}
+}
---
+++ Back-end/src/main/java/com/attendanceTracking/service/AttendanceService.java
@@ -0,0 +1,29 @@
+package com.attendanceTracking.service;
+
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.attendanceTracking.model.AttendanceModel;
+
+import com.attendanceTracking.repository.AttendanceRepo;
+
+@Service
+public class AttendanceService {
+	
+	@Autowired
+	private AttendanceRepo repo;
+
+
+	
+	  public Optional<AttendanceModel> showAll(int id) { 
+		  return repo.findById(id); 
+		  }
+	 
+	  public void addAttendance(AttendanceModel AttendanceModel) {
+		  repo.save(AttendanceModel);
+
+	    }
+}
